489a6ce6773741509c2c6c9adf7012095d6a5b95
Maciej Goszczycki <mgoszcz2@gmail.com>
2015-06-18 16:04:57 +0100

    Working basic version

--- src/Main.hs
+++ src/Main.hs
@@ -1,33 +1,57 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 import Github.Repos
+import Github.Issues
 import Github.Auth
 import Data.Aeson
-import Control.Monad.Trans.Either
-import Control.Monad (liftM)
-import Text.Printf (printf)
-import Data.Time.Clock (getCurrentTime, UTCTime)
+import Data.Time.Clock
 import Data.List (sort)
+import Text.Printf (printf)
+import Control.Monad (liftM)
 import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
 import qualified Data.ByteString.Lazy as BS
 
-data Results = Results UTCTime String [Contributor] deriving (Show)
+data Results = Results UTCTime String [Contributor] [Issue] deriving (Show)
 
 instance ToJSON Contributor where
-    toJSON (KnownContributor ccnt avatar username _ _ _) = object ["avatar" .= avatar, "login" .= username, "count" .= ccnt ]
-    toJSON (AnonymousContributor ccnt username) = object ["avatar" .= anonUrl, "login" .= username, "count" .= ccnt ]
+    toJSON (KnownContributor ccnt avatar username _ _ _) =
+        object ["avatar" .= avatar, "login" .= username, "count" .= ccnt, "url" .= userUrl username ]
+    toJSON (AnonymousContributor ccnt username) =
+        object ["avatar" .= anonUrl, "login" .= username, "count" .= ccnt, "url" .= userUrl username]
         where anonUrl = "#" :: String
 
+instance ToJSON Issue where
+    toJSON Issue{..} =
+        object [ "url" .= issueHtmlUrl
+               , "number" .= issueNumber
+               , "title" .= issueTitle
+               , "created" .= fromGithubDate issueCreatedAt]
+
 instance ToJSON Results where
-    toJSON (Results time org contribs) = object ["time" .= time, "name" .= org, "contributors" .= contribs]
+    toJSON (Results time org contribs issues) =
+        object ["time" .= time,"name" .= org, "contributors" .= contribs, "issues" .= issues] --FIXME Opening user
+
+userUrl :: String -> String
+userUrl = printf "https://github.com/%s/"
 
 githubAuth :: Maybe GithubAuth
 githubAuth = Just $ GithubOAuth "d9e6ce7ba9378a580e564e9dd4ecd3b4e1eb20b8"
 
-eitherIO :: Show e => Either e a -> IO a
-eitherIO (Left e) = print e >> exitFailure
-eitherIO (Right a) = return a
+eitherIO :: IO (Either e a) -> IO a
+eitherIO = eitherIOCnt 1
+
+eitherIOCnt :: Int -> IO (Either e a) -> IO a
+eitherIOCnt cnt io
+    | attempts < cnt = hPutStrLn stderr (printf "Gave up after %d attempts!" attempts) >> exitFailure
+    | otherwise = do res <- io
+                     case res of
+                          (Right a) -> return a
+                          (Left _) -> do hPutStrLn stderr (printf "Retrying request! (%d/%d)" cnt attempts)
+                                         eitherIOCnt (cnt + 1) io
+    where attempts = 5
 
 getName :: Contributor -> String
 getName (KnownContributor _ _ name _ _ _) = name
@@ -49,17 +73,26 @@ nubContributors cs@(c:cr) = updateContributions c cnts : nubContributors (filter
                                     then rc + getContributions u
                                     else rc) 0 cs
 
+blankLine :: IO ()
+blankLine = putStrLn ""
+
 main :: IO ()
 main = do
     now <- getCurrentTime
-    putStrLn "Getting repo list.."
-    out <- liftM (Results now organization) $ runEitherT names >>= eitherIO
+    printf "Generating data for '%s'\n" organization
+    repos <- eitherIO $ putStrLn "Getting repo list.." >> organizationRepos' githubAuth organization
+    blankLine
+    issues <- getInfo getIssues concat repos
+    contribs <- getInfo getContribs (sort . nubContributors . concat) repos
+    blankLine
     printf "Saving data to '%s'\n" saveFileName
-    BS.writeFile saveFileName $ encode out
-    putStrLn "Done."
-    where repos = EitherT $ organizationRepos' githubAuth organization
-          names = liftM (sort . nubContributors . concat) $ repos >>= mapM (EitherT . getContribs)
-          organization = "evercam"
+    BS.writeFile saveFileName . encode $ Results now organization contribs issues
+    printf "Found %d issues and %d contributors\n" (length issues) (length contribs)
+    finish <- getCurrentTime
+    printf "Done! Took %s\n" . show $ diffUTCTime finish now
+    where getInfo get cleanup repos = liftM cleanup $ mapM (eitherIO . get . repoName) repos
+          getContribs name = printf "Contributors to %s..\n" name >> contributors' githubAuth organization name
+          getIssues name = printf "Open issues of %s..\n" name >> issuesForRepo' githubAuth organization name issueLimitations
+          issueLimitations = []
+          organization = "Cambatv"
           saveFileName = "saved.json"
-          getContribs repo = let name = repoName repo
-                             in printf "Checking %s..\n" name >> contributors' githubAuth organization name
